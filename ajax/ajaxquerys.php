82ab6f9ffc9a7e5bd3cfc37b5caf8d8a2b2b9247
Matias <queiroloco@gmail.com>
2019-06-05 17:35:14 -0400

    vardump $like

--- ajax/ajaxquerys.php
+++ ajax/ajaxquerys.php
@@ -179,7 +179,6 @@ switch ($action) {
 						cat.name,
 						u.id as teacherid,
 						CONCAT( u.firstname, ' ', u.lastname) as teacher
-						
 						FROM mdl_user u
 						INNER JOIN {user_enrolments} ue ON (ue.userid = u.id)
 						INNER JOIN {enrol} e ON (e.id = ue.enrolid)
@@ -194,7 +193,7 @@ switch ($action) {
 		}
 		$parametros = array_merge($parametros1, $filter);
 		$courses = $DB->get_records_sql($sqlcourses, $parametros);
-	
+		var_dump($like);
 		echo json_encode($courses);
 		break;
 	//This case returns the course data to add it to the cart list
