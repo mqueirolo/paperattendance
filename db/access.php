8d00f84b844b39fb26268190432f0f4b547de46d
Loreto  Maria Lopez Pino <loreto.lopez.p@uai.cl>
2019-07-02 11:43:36 -0400

    Fix tabulation

--- db/access.php
+++ db/access.php
@@ -123,20 +123,20 @@ $capabilities = array(
 						'manager' => CAP_ALLOW
 				)),
         'local/paperattendance:takeattendance' => array(
-            'captype' => 'write',
-            'contextlevel' => CONTEXT_COURSE,
-            'archetypes' => array(
-                'student' => CAP_PROHIBIT,
-                'teacher' => CAP_ALLOW,
-                'editingteacher' => CAP_ALLOW,
-                'manager' => CAP_ALLOW
-            )),
+                'captype' => 'write',
+                'contextlevel' => CONTEXT_COURSE,
+                'archetypes' => array(
+                    'student' => CAP_PROHIBIT,
+                    'teacher' => CAP_ALLOW,
+                    'editingteacher' => CAP_ALLOW,
+                    'manager' => CAP_ALLOW
+                )),
         'local/paperattendance:adminacademic' => array(
-            'riskbitmask' => RISK_MANAGETRUST,
-            'captype' => 'write',
-            'contextlevel' => CONTEXT_SYSTEM,
-            'archetypes' => array(
-                'manager' => CAP_ALLOW
-            ))
+                'riskbitmask' => RISK_MANAGETRUST,
+                'captype' => 'write',
+                'contextlevel' => CONTEXT_SYSTEM,
+                'archetypes' => array(
+                    'manager' => CAP_ALLOW
+                ))
 		
 );
